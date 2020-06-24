8fbcb7c15d09993de87ec31800b5c43063310193
Maja <majam1616@gmail.com>
2020-06-24 14:18:34 -0400

    Updated AV_experiment

--- AV_experiment.py
+++ AV_experiment.py
@@ -1,14 +1,14 @@
 from psychopy import data, visual, core, monitors
-#import itertools
 import v2_flash_stim as fs2
 import random 
 import numpy as np
 import tdt
 
+#TODO: change flash_dur -> frames, do so by looping for n frames (for frame in range 0 to n, win.flip()...)
+
 
 class Experiment():
-
-	def __init__(self, numTrials=None, ISI=None, flash_dur=None, luminance=None, wave_freq=None,pulse_dur=None, wave_amp=None, auditory_on=None):
+	def __init__(self, numTrials=None, ISI=None, flash_dur=None, luminance=None, wave_freq=None,pulse_dur=None, wave_amp=None, stimulus=None):
 
 		self.numTrials=numTrials #number of trials
 		self.ISI=ISI #interstimulus interval (time btw flashes, in seconds, for the flash stimulus)
@@ -21,7 +21,7 @@ class Experiment():
 		self.wave_freq=wave_freq #frequency (Hz)
 		self.pulse_dur=pulse_dur #length of pulse (sound plays)
 		self.wave_amp=wave_amp #volume of the sound
-		self.auditory_on=auditory_on #boolean; determines whether there is an auditory component or not
+		self.stimulus=stimulus #boolean; determines whether there is an auditory component or not
 
 	#takes in window parameters which can be changed as needed; provides flexibility
 	def run_experiment(self, window_dim=[2560, 1600], screen_index=0, monitor_name="testMonitor"):
@@ -46,7 +46,7 @@ class Experiment():
 				"wave_freq": self.wave_freq, #waveFreq = freq of the tone
 				"pulse_dur": self.pulse_dur, #pulseDur = length of time the tone is played
 				"wave_amp": self.wave_amp, #waveAmp = sound levels
-				"auditory_on": self.auditory_on
+				"stimulus": self.stimulus
 				}
 
 		stimList = data.createFactorialTrialList(factors)
@@ -64,43 +64,47 @@ class Experiment():
 		#making experimentHandler and trials
 		exp = data.ExperimentHandler(name='testExp', savePickle=True, saveWideText=True, dataFileName='experiment')
 
-		trials = data.TrialHandler(trialList=trials_subset, nReps=3, method='sequential') # since already randomized we can go ahead and use "sequential" here...
+		trials = data.TrialHandler(trialList=trials_subset, nReps=1, method='sequential') # since already randomized we can go ahead and use "sequential" here...
 
 		exp.addLoop(trials)
 
-
-		#adding the data that we are collecting
-		#trials.data.addDataType('Auditory Response')
-		#trials.data.addDataType('Visual Response')
-
-		#the experiment loop!
+		#the experiment loop! 0: Auditory only; 1: Visual only; 2: A+V
 		for trial in trials:
 			inter=trial['ISI']
 
-		#	print(trial.items()) #access it like a dict
-			if(trial['auditory_on']==True):
-				print("===Auditory component on===\n *Play tone at %d dB*\n" %trial['wave_amp'])
-				print("WF audio: " , trial['wave_freq'])
+			if(trial['stimulus']==0): #auditory stim only
+				print("===Auditory component only===\n *Play tone at %d dB*\n" %trial['wave_amp'])
 				core.wait(inter)
 
-			else:
-				trial['wave_freq']=None  # or "" looks cleaner in excel...but None probably better for loading into python
+			elif(trial['stimulus']==1): #visual stim only 
+				trial['wave_freq']=None  # set all audio-related parameters to none 
 				trial['pulse_dur']=None
 				trial['wave_amp']=None
 
+				print("===Visual component only===\n")
+				dur = trial['flash_dur'] #pres. dur.
+				lum = trial['luminance'] # luminance
 
-			print("===No auditory===\n")
-			dur = trial['flash_dur'] #pres. dur.
-			lum = trial['luminance'] # luminance
-			print("WF no audio: " , trial['wave_freq'])
+				flash.pres_dur=dur
+				flash.luminance=lum
+				print("Luminance: ", lum)
+				#present the stimulus
+				flash.flash(window)
+				window.flip()
+				core.wait(inter)
 
-			flash.pres_dur=dur
-			flash.luminance=lum
-			print("Luminance: ", lum)
+			elif(trial['stimulus']==2): #A+V stim
+				print("===Auditory and visual components on===\n *Play tone at %d dB*\n" %trial['wave_amp'])
+				dur = trial['flash_dur'] #pres. dur.
+				lum = trial['luminance'] # luminance
 
-			flash.flash(window)
-			window.flip()
-			core.wait(inter)
+				flash.pres_dur=dur
+				flash.luminance=lum
+				print("Luminance: ", lum)
+				#present the stimulus
+				flash.flash(window)
+				window.flip() #TODO: change to be timed by frame instead of seconds
+				core.wait(inter)
 
 			exp.nextEntry()
 
@@ -108,7 +112,7 @@ class Experiment():
 def main():
 	#Parameters:
 	numTrials = 10
-	ISI = [2, 3, 4, 5] #ISI in seconds
+	ISI = [2.0, 3.0, 4.0, 5.0] #ISI in seconds
 
 	#flash parameters
 	flash_dur = [.001, .002] #flash durs in seconds (100 ms, 200 ms)
@@ -117,13 +121,16 @@ def main():
 	#auditory parameters
 	frequency = [1.1, 2.2, 3.3, 4.4] #waveFreq in TDT
 	duration = [.001, .002, .003, .004] # in seconds; pulseDur in TDT
-	sound_levels = [20, 40, 60, 80] # dB; waveAmp in TDT
+	sound_levels = [20.0, 40.0, 60.0, 80.0] # dB; waveAmp in TDT
 
 	#Auditory on (T/F? if T then A+V, if F then Visual only)
-	auditory_on = [True, False]				
+	stims={ 0: "auditory_only", 
+			1: "visual_only",
+			2: "A+V"
+		}		
 
 
-	exper = Experiment(numTrials=numTrials, ISI=ISI, flash_dur=flash_dur, luminance=luminance, wave_freq=frequency,pulse_dur=duration, wave_amp=sound_levels, auditory_on=auditory_on)
+	exper = Experiment(numTrials=numTrials, ISI=ISI, flash_dur=flash_dur, luminance=luminance, wave_freq=frequency,pulse_dur=duration, wave_amp=sound_levels, stimulus=stims)
 	exper.run_experiment()
 
 if __name__ == '__main__':
