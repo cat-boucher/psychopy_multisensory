6f59a178e19a361aaf46018011e137b5255edad8
Maja <majam1616@gmail.com>
2020-07-06 17:04:38 -0400

    Integrated with synapse api

--- AV_experiment.py
+++ AV_experiment.py
@@ -3,6 +3,9 @@ import v2_flash_stim as fs2
 import random 
 import numpy as np
 import PhotodiodeMarker as pdm
+import time
+import tdt
+
 
 #TODO: change flash_dur -> frames, do so by looping for n frames (for frame in range 0 to n, win.flip()...)
 
@@ -23,12 +26,18 @@ class Experiment():
 		self.wave_amp=wave_amp #volume of the sound
 		self.stimulus=stimulus #boolean; determines whether there is an auditory component or not
 
-		self.delay=delay #delay parameter to be read from TDT? Between presentation of auditory and visual stimulus. In ms
+		self.delay=delay #Between presentation of auditory and visual stimulus within the trial. In ms
 
 		self.pre_stim = 8 # initial wait time before the stimulus presentation starts
 
 	#takes in window parameters which can be changed as needed; provides flexibility
 	def run_experiment(self, window_dim=[800, 600], screen_index=0, monitor_name="testMonitor"):
+
+		#temporary, just for testing bc i get errors since I dont have synapse. set to true if actually connecting to synapse
+		CONNECT = False
+		if(CONNECT):
+			# Connect to Synapse
+			syn = tdt.SynapseAPI()
 
 		#defining the window
 		window=visual.Window(size=window_dim, 
@@ -50,7 +59,8 @@ class Experiment():
 				"wave_freq": self.wave_freq, #waveFreq = freq of the tone
 				"pulse_dur": self.pulse_dur, #pulseDur = length of time the tone is played
 				"wave_amp": self.wave_amp, #waveAmp = sound levels
-				"stimulus": self.stimulus
+				"stimulus": self.stimulus,
+				"delay": self.delay # delay between presenting A and V
 				}
 
 		stimList = data.createFactorialTrialList(factors)
@@ -81,25 +91,34 @@ class Experiment():
 			if n<10: #draw the marker for 10 frames
 				marker.draw_marker(window)
 			
-			#	print('marker')
-		#	else:
-			#	print("waiting")
-
-	#	print("Clearing")
 		window.flip()
 
 		#the experiment loop! 0: Auditory only; 1: Visual only; 2: A+V
 		for trial in trials:
 			inter=trial['ISI']
+			if(CONNECT):
+					#set the auditory value decided by Psychopy in Synapse: WaveAmp, WaveFreq, Delay (?)
+					syn.setParameterValue('aStim2', 'WaveAmp', trial['wave_amp'])
+					syn.setParameterValue('aStim2', 'WaveFreq', trial['wave_freq'])
+					syn.setParameterValue('aStim2', 'PulseDur', trial['pulse_dur'])
+
 
 			if(trial['stimulus']==0): #auditory stim only
+
 				print("===Auditory component only===\n *Play tone at %d dB*\n" %trial['wave_amp'])
-				core.wait(inter)
+				trial['delay']=None
 
 			elif(trial['stimulus']==1): #visual stim only 
 				trial['wave_freq']=None  # set all audio-related parameters to none 
 				trial['pulse_dur']=None
 				trial['wave_amp']=None
+				trial['delay']=None
+				if(CONNECT):
+					#set the auditory value decided by Psychopy in Synapse: WaveAmp, WaveFreq, Delay (?)
+					syn.setParameterValue('aStim2', 'WaveAmp', 0) # set all values to 0  bc no auditory
+					syn.setParameterValue('aStim2', 'WaveFreq', 0)
+					syn.setParameterValue('aStim2', 'PulseDur', 0)
+
 
 				print("===Visual component only===\n")
 				dur = trial['flash_dur'] #pres. dur.
@@ -108,43 +127,96 @@ class Experiment():
 				flash.pres_dur=dur
 				flash.luminance=lum
 
-			
-
 				print("Luminance: ", lum)
 				#present the stimulus
 				flash.flash(window)
 				marker.draw_marker(window)
 				window.flip()
-				core.wait(inter)
-				window.flip()
+			#	core.wait(inter)
+			#	window.flip()
+
+
 
 			elif(trial['stimulus']==2): #A+V stim
-				print("===Auditory and visual components on===\n *Play tone at %d dB*\n" %trial['wave_amp'])
-				dur = trial['flash_dur'] #pres. dur.
-				lum = trial['luminance'] # luminance
+				#option 1: Audio first: delay -
+				if(trial['delay'] < 0): #delay = time between A and V within the trial
 
-				flash.pres_dur=dur
-				flash.luminance=lum
+					# wait while the audio is going + the delay btw stimuli
+					total_wait = abs(trial['delay'])+trial['pulse_dur'] 
+					core.wait(total_wait)
+
+					print("===Auditory and visual components on===\n *Play tone at %d dB*\n" %trial['wave_amp'])
+					dur = trial['flash_dur'] #pres. dur.
+					lum = trial['luminance'] # luminance
+
+					flash.pres_dur=dur
+					flash.luminance=lum
+
+					print(trial['delay'])
+
+					print("Luminance: \n", lum)
+					#present the stimulus
+					flash.flash(window)
+					marker.draw_marker(window)
+					window.flip() #TODO: change to be timed by frame instead of seconds
+				#	core.wait(inter)
+				#	window.flip()
+
+				#option 2: Visual first: delay +
+				if(trial['delay'] > 0):
+
+					print("===Auditory and visual components on===\n *Play tone at %d dB*\n" %trial['wave_amp'])
+					dur = trial['flash_dur'] #pres. dur.
+					lum = trial['luminance'] # luminance
+
+					flash.pres_dur=dur
+					flash.luminance=lum
 
 
-				print("Luminance: ", lum)
-				#present the stimulus
-				flash.flash(window)
-				marker.draw_marker(window)
-				window.flip() #TODO: change to be timed by frame instead of seconds
-				core.wait(inter)
+					print("Luminance: \n", lum)
+					#present the stimulus
+					flash.flash(window)
+					marker.draw_marker(window)
+					window.flip() #TODO: change to be timed by frame instead of seconds
+				#	core.wait(inter)
+				#	window.flip()
+
+					total_wait = trial['delay']+trial['pulse_dur'] # after visual presented, need to wait the delay btw stimuli + the duration of the audio pulse
+					core.wait(total_wait)
+
+
+				#option 3: Simultaneous: delay 0 
+				if(trial['delay'] == 0):
+					print("===Auditory and visual components on===\n *Play tone at %d dB*\n" %trial['wave_amp'])
+					dur = trial['flash_dur'] #pres. dur.
+					lum = trial['luminance'] # luminance
+
+					flash.pres_dur=dur
+					flash.luminance=lum
+
+					print("Luminance: \n", lum)
+					#present the stimulus
+					flash.flash(window)
+					marker.draw_marker(window)
+					window.flip() #TODO: change to be timed by frame instead of seconds
+				#	core.wait(inter)
+				#	window.flip()
+
+				core.wait(inter) #inter stimulus interval = time between successive presentations
 				window.flip()
 
+			time.sleep(1) #not sure how much to sleep yet: maximum delay if presenting auditory before visual plus maybe 30-50%
 			exp.nextEntry()
 
 
 def main():
 	#Parameters:
 	numTrials = 10
-	ISI = [2.0, 3.0, 4.0, 5.0] #ISI in seconds
+	ISI = [2.0, 3.0, 4.0, 5.0] #ISI in seconds: time between trials
+	delay = [-50.0, -25.0, -10.0, 0.0, 10.0, 25.0, 50.0] # *change to ms* ; time between presentation of A + V within a trial. (-) A first, (+) V first 
 
 	#flash parameters
-	flash_dur = [.001, .002] #flash durs in seconds (100 ms, 200 ms)
+	flash_dur = [.001, .002, .1] #flash durs in seconds (100 ms, 200 ms)
 	luminance = [[1,1,1], [.86, .86, .86], [0,.1,1]] #white , grayish, purple just for testing
 
 	#auditory parameters
@@ -158,7 +230,7 @@ def main():
 		}		
 
 
-	exper = Experiment(numTrials=numTrials, ISI=ISI, flash_dur=flash_dur, luminance=luminance, wave_freq=frequency,pulse_dur=duration, wave_amp=sound_levels, stimulus=stims)
+	exper = Experiment(numTrials=numTrials, ISI=ISI, flash_dur=flash_dur, luminance=luminance, wave_freq=frequency,pulse_dur=duration, wave_amp=sound_levels, stimulus=stims, delay=delay)
 	exper.run_experiment()
 
 if __name__ == '__main__':
